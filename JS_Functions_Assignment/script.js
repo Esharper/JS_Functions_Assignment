43c8dc6f1d05a430fa2965283a4d5e63255cf319
Esharper <147178117+Esharper@users.noreply.github.com>
2023-11-11 00:43:46 -0600

    Add files via upload

---
+++ JS_Functions_Assignment/script.js
@@ -0,0 +1,69 @@
+'use strict'
+
+alert("Math 2.5");
+alert("welcome to my workshop")
+console.log(alert);
+
+ document.write('Elgins World');
+
+let myArray=[1,2,3,4];
+console.log(myArray);
+console.log(myArray.pop());
+console.log(myArray);
+
+console.log(docuwrite);
+
+function docuwrite(hello, my , name, is, ELGIN) {
+    docuwrite('hello');
+    docuwrite('my name is ');
+    docuwrite('ELGIN');
+}
+
+
+function calculateAreaOfRectangle(length , width = area) {
+    return length * width;
+  }
+  console.log(calculateAreaOfRectangle);
+  
+  let area = calculateAreaOfRectangle(5,2);
+
+  console.log(area);
+
+  console.log(calculateAreaOfRectangle(5, 2));
+  // Expected output: 10
+  
+  console.log(calculateAreaOfRectangle(5));
+  // Expected output: 5
+
+calculateAreaOfRectangle("length, width, =, area");
+console.log(calculateAreaOfRectangle)
+
+  function convertFahrenheitToCelsius(temperatureInFahrenheit) {
+    let celsius = (temperatureInFahrenheit - 32) * (5 / 9);
+    return celsius;
+}
+   convertFahrenheitToCelsius("temperatureInFahrenheit");
+   console.log(convertFahrenheitToCelsius);
+   
+   
+
+let temperatureInFahrenheit = 32; 
+let temperatureInCelsius = convertFahrenheitToCelsius(temperatureInFahrenheit);
+console.log(temperatureInCelsius);
+
+function greetUser(Rufus) {
+    let greeting = "Hello, " + Rufus + "! Welcome!";
+    return greeting;
+}
+console.log(greetUser);
+
+let userName = "Rufus";
+let userGreeting = greetUser(userName);
+
+greetUser("Rufus");
+console.log(userGreeting);
+
+
+  
+    
+
